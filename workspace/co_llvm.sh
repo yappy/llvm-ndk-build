38b92499db29ecf3cc8e1f91da80a81cc35c8011
yappy <superunyo@yahoo.co.jp>
2018-02-19 19:12:55 +0900

    Checkout clang C front-end

--- workspace/co_llvm.sh
+++ workspace/co_llvm.sh
@@ -8,3 +8,4 @@ TAG="tags/RELEASE_501/final"
 #TAG="tags/RELEASE_600/final"
 
 svn co "https://llvm.org/svn/llvm-project/llvm/${TAG}" ${SRC_DIR}
+svn co "http://llvm.org/svn/llvm-project/cfe/${TAG}" "${SRC_DIR}/tools/clang"
